e32b9d4ba912e8a42a5d59d6429754d68a1f70b6
Chris <chrisconcord@gmail.com>
2023-11-02 17:48:52 -0400

    Pokemon api doesn't line up exactly, start moving towards using the site filters

--- bot/bot.py
+++ bot/bot.py
@@ -1,9 +1,5 @@
-# import pokebase as pb
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
-# init pokemon information
-
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
@@ -22,13 +18,19 @@ correct = False
 while guess_number < 8 and not correct:
     guess = guess_list[guess_number]
     guess_number += 1
+    make_guess(guess)
+    
+# print("guess text: " + guesses.text)
+
+def make_guess(guess):
+    feedback = {}
     w_input_box.send_keys(guess)
     w_input_box.submit()
     driver.implicitly_wait(2)
     guess_list = w_guesses.find_elements(by=By.ID, value='guess*')
     for e in guess_list:
-        print (guess_list)
-    
-# print("guess text: " + guesses.text)
+        print (e)
 
-
+    return feedback
+def read_guess(guess_number):
+    w_guess_row = driver.find_element(by=By.ID, value="guess" + guess_number)
